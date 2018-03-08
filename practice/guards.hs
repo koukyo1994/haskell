3107cb3da113a63aa0dbb87673c7200faa157201
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-03-09 01:40:05 +0900

    By the end of 6th

--- practice/guards.hs
+++ practice/guards.hs
@@ -1,4 +1,11 @@
+-- safeSqrt x = sqrtBool (x >= 0) x
+-- sqrtBool True x = Just (sqrt x)
+-- sqrtBool False _ = Nothing
 
-safeSqrt x = sqrtBool (x >= 0) x
-sqrtBool True x = Just (sqrt x)
-sqrtBool False _ = Nothing
+safeSqrt x
+    | x >= 0 = Just (sqrt x)
+    | otherwise = Nothing
+
+div3 x
+    | (x `mod` 3 == 0) = x `div` 3
+    | otherwise = x
